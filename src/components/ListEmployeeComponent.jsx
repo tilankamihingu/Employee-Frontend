1e4abc8486467f16b84013b491103c998cadd0e5
tilankamihingu <tilankamihingu2018@gmail.com>
2021-11-11 23:00:13 +0530

    update complete

--- src/components/ListEmployeeComponent.jsx
+++ src/components/ListEmployeeComponent.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom';
 import EmployeeService from '../services/EmployeeService';
 import './EmpList.css';
 
--- src/components/UpdateEmployeeComponent.jsx
+++ src/components/UpdateEmployeeComponent.jsx
@@ -3,12 +3,11 @@ import React, { Component } from 'react'
 import EmployeeService from '../services/EmployeeService';
 
 class UpdateEmployeeComponent extends Component {
-
     constructor(props){
         super(props)
 
         this.state = {
-            id: this.props.match.params.id, 
+            id: this.props.match.params.id,
             name: '',
             email: '',
             jobTitle: '',
@@ -21,31 +20,29 @@ class UpdateEmployeeComponent extends Component {
         this.changePhoneHandler = this.changePhoneHandler.bind(this);
         this.changeImageHandler = this.changeImageHandler.bind(this);
         this.updateEmployee = this.updateEmployee.bind(this);
-
     }
-    
+
     componentDidMount(){
         EmployeeService.getEmployeeById(this.state.id).then((res) =>{
-            let employee = res.data;
-            this.setState({name: employee.name,
+            let employee = res.data
+            this.setState({
+                name: employee.name,
                 email: employee.email,
                 jobTitle: employee.jobTitle,
                 phone: employee.phone,
                 imageUrl: employee.imageUrl
-            });
+            })
         });
     }
 
     updateEmployee = (e) =>{
         e.preventDefault();
-        let employee = {name: this.state.name, email: this.state.email, jobTitle: this.state.jobTitle, phone: this.state.phone, imageUrl: this.state.imageUrl};
+        let employee = {id:this.state.id,name: this.state.name, email: this.state.email, jobTitle: this.state.jobTitle, phone: this.state.phone, imageUrl: this.state.imageUrl};
         console.log('employee =' + JSON.stringify(employee));
 
         EmployeeService.updateEmployee(employee, this.state.id).then(res => {
             this.props.history.push('/');
         });
-        
-
     }
 
     changeNameHandler = (event) =>{
@@ -78,16 +75,16 @@ class UpdateEmployeeComponent extends Component {
                 <div className="right-side">
                     <form>
                         <h1>Name</h1>
-                        <input type="text" required placeholder="name" name="name" value={this.state.name} onChange={this.changeNameHandler} />
+                        <input type="text" required placeholder="name" value={this.state.name} onChange={this.changeNameHandler} />
                         <h1>Email</h1>
-                        <input type="text" required placeholder="email" name="email" value={this.state.email} onChange={this.changeEmailHandler} />
+                        <input type="text" required placeholder="email" value={this.state.email} onChange={this.changeEmailHandler} />
                         <h1>Job Title</h1>
-                        <input type="text" required placeholder="job title" name="jobTitle" value={this.state.jobTitle} onChange={this.changeJobHandler} />
+                        <input type="text" required placeholder="job title" value={this.state.jobTitle} onChange={this.changeJobHandler} />
                         <h1>Phone</h1>
-                        <input type="text" required placeholder="phone number" name="phone" value={this.state.phone} onChange={this.changePhoneHandler} />
+                        <input type="text" required placeholder="phone number" value={this.state.phone} onChange={this.changePhoneHandler} />
                         <h1>Image Url</h1>
-                        <input type="text" required placeholder="image url" name="imageUrl" value={this.state.imageUrl} onChange={this.changeImageHandler} />
-                        <button onClick={this.updateEmployee} className="send">Send</button>
+                        <input type="text" required placeholder="image url" value={this.state.imageUrl} onChange={this.changeImageHandler} />
+                        <button onClick={this.updateEmployee} className="send">Save</button>
                     </form>
                 </div>
             </div>
@@ -95,5 +92,6 @@ class UpdateEmployeeComponent extends Component {
         )
     }
 }
+   
 
 export default UpdateEmployeeComponent
